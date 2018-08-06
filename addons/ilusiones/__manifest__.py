1b7a4bf27d6bd7f3047e47cb945d0a499695d412
luis-cordova <cnpoe@fedora.luis>
2018-08-06 05:27:57 -0500

    added first files for project

---
+++ addons/ilusiones/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Ilusiones Application',
+    'description':
+    '''
+        Custom store application.
+        3 types of sale for Ilusiones S.A. de C.V.
+    ''',
+    'author': 'Luis Morales',
+    'website': 'https://www.github.com/cnpoe/',
+    'depends': [
+        'base',
+    ],
+    'application': True,
+    'summary': 'Custom application for Ilusiones S.A de C.V. odoo 11',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'installable': True,
+    'category': 'Test',
+    'data': [
+    ],
+}
