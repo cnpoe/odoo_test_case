3caa8b757f28136f56f75abc7fcdbb218abcec0b
luis-cordova <cnpoe@fedora.luis>
2018-08-09 00:53:41 -0500

    moving line methods to order line class

--- addons/ilusiones/models/sale_order.py
+++ addons/ilusiones/models/sale_order.py
@@ -4,50 +4,9 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def prepare_line(self, product, order_id):
-        """Generates a order line with the required parameters.
-
-        Arguments:
-            product {product.product} -- Product of the order line
-            order_id {int} -- Order id
-
-        Returns:
-            dict -- Order line dictionary with required parameters
-        """
-        line = {
-            'layout_category_id': False,
-            'product_uom': product.uom_id.id,
-            'name': product.name,
-            'route_id': False,
-            'product_uom_qty': 1,
-            'tax_id': [[6, False, [2]]],
-            'order_id': order_id,
-            'product_id': product.id,
-            'price_unit': 0,
-            'active': False,
-            'in_combo': True,
-        }
-        return line
-
-    def create_order_lines(self, order_line):
-        """Writes order lines for each order line with a product combo.
-
-        Arguments:
-            order_line {sale.order.line} -- Order lines of sale order.
-        """
-        order_line_env = self.env['sale.order.line']
-        if (order_line.product_id.type == 'combo' or
-                order_line.product_id.type == 'Combo'):
-            for product in order_line.product_id.product_combo.sale_products:
-                if product.type == 'product':
-                    values = self.prepare_line(product, order_line.order_id.id)
-                    order_line_env.create(values)
-
     @api.model
     def create(self, vals):
         result = super(SaleOrder, self).create(vals)
-        for order_line in result.order_line:
-            self.create_order_lines(order_line)
         return result
 
     @api.multi
@@ -58,4 +17,6 @@ class SaleOrder(models.Model):
             lines = order_line_env.with_context(active_test=False).search([
                 ('order_id', '=', order.id)
             ])
+            print('**************************+')
+            print(lines)
             lines._action_launch_procurement_rule()
--- addons/ilusiones/models/sale_order_line.py
+++ addons/ilusiones/models/sale_order_line.py
@@ -1,13 +1,57 @@
 from odoo import models, fields, api
 
 
-class SaleOderLine(models.Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     active = fields.Boolean(default=True)
     in_combo = fields.Boolean(default=False)
 
+    def prepare_line(self, product, order_id):
+        """Generates a order line with the required parameters.
+
+        Arguments:
+            product {product.product} -- Product of the order line
+            order_id {int} -- Order id
+
+        Returns:
+            dict -- Order line dictionary with required parameters
+        """
+        line = {
+            'layout_category_id': False,
+            'product_uom': product.uom_id.id,
+            'name': product.name,
+            'route_id': False,
+            'product_uom_qty': 1,
+            'tax_id': [[6, False, [2]]],
+            'order_id': order_id,
+            'product_id': product.id,
+            'price_unit': 0,
+            'active': False,
+            'in_combo': True,
+        }
+        return line
+
+    def create_order_lines(self, order_line):
+        """Writes order lines for each order line with a product combo.
+
+        Arguments:
+            order_line {sale.order.line} -- Order lines of sale order.
+        """
+        order_line_env = self.env['sale.order.line']
+        print('---------------------------------')
+        if (order_line.product_id.type == 'combo' or
+                order_line.product_id.type == 'Combo'):
+            print('is combo')
+            print(order_line.product_id.product_combo.sale_products)
+            for product in order_line.product_id.product_combo.sale_products:
+                if product.type == 'product':
+                    values = self.prepare_line(product, order_line.order_id.id)
+                    order_line_env.create(values)
+
     @api.model
-    def create(self, vals):
-        result = super(SaleOderLine, self).create(vals)
-        return result
+    def create(self, values):
+        values.update(self._prepare_add_missing_fields(values))
+        line = super(SaleOrderLine, self).create(values)
+        self.create_order_lines(line)
+        return line
